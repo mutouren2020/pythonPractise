80bb1fb638c237500f5a284a240f461c7f3ed398
mutouren <lin.tian@aliyun.com>
2020-01-16 00:10:45 +0800

    paratise

---
+++ sheet02/digital.py
@@ -0,0 +1,6 @@
+aa = 23
+print("tianlin age:" + str(aa))
+
+# 这是注释
+print("3/2:"+str(3/2))
+print("5%2:"+str(5%2))
---
+++ sheet02/string01.py
@@ -0,0 +1,4 @@
+message=" python "
+print("去除两端空字符串：" + message.strip()+"end")
+print("去除头部空字符串："+message.lstrip()+"end")
+print("去除尾部空字符串："+message.rstrip()+"end")
